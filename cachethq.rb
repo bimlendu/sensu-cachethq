9914359fe70c7231fabc34551a1fbde2e598a602
Bimlendu Mishra <justbimlendu@gmail.com>
2015-04-08 08:27:04 +0530

    Update cachethq.rb

--- cachethq.rb
+++ cachethq.rb
@@ -9,8 +9,7 @@
 # Copyright 2015 Bimlendu Mishra <justbimlendu@gmail.com>
 #
 # Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
-# 
+# for details. https://github.com/sensu/sensu-community-plugins/blob/master/LICENSE
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
